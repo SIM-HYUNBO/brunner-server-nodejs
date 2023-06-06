6a7430c79bde11f39adff259146188a0ccbb23df
Hyunbo Sim <hbsim0605@gmail.com>
2023-06-06 23:26:11 +0900

    no changes

--- components/database/sqls/TB_COR_USER_MST.js
+++ components/database/sqls/TB_COR_USER_MST.js
@@ -3,7 +3,8 @@
 export const select_DB_COR_USER_MST_01 = 
 `SELECT SYSTEM_CODE, USER_ID, PASSWORD, USER_NAME
   FROM brunner.tb_cor_user_mst
- WHERE USER_ID = ?`
+ WHERE USER_ID = ?
+ `
  ;
 
  export const update_DB_COR_USER_MST_01 = 
