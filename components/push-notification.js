d6e70778dc05eabfee4f36dc75462e004d2723ba
Hyunbo Sim <hbsim0605@gmail.com>
2023-07-22 17:56:01 +0900

    fcm notification

--- components/push-notification.js
+++ components/push-notification.js
@@ -2,12 +2,15 @@
 
 import * as database from './database/database'
 import * as TB_COR_NOTIFICATION_ITEM from './database/sqls/TB_COR_NOTIFICATION_ITEM'
+import admin from "firebase-admin";
+import serviceAccount from "./notification/brunner-push-7e0ef-firebase-adminsdk-ptyec-3892a26b45.json";
 
 /* 사용자에게 푸시 알람을 생성함
 1. 신규 대화항목를 만들었거나 기존 대화항목을 편집했을때 
  - 작성자 본인을 제외하고 해당 메인 대화에 댓글을 달았거나 메인 대화를 작성한 사람에게 (toUserIds)
 2.특정 사용자에게 메시지를 직접 보내고자 할 경우
 */
+let initialized = false;
 
 export default async function createPushNotification(promisePool, jPushItem) {
     const fromSource = jPushItem.fromSource; // TALK_ITEM
@@ -26,6 +29,7 @@ export default async function createPushNotification(promisePool, jPushItem) {
                 fromSource
             ]).then((result) => {
                 console.log(`==========================\nRESULT:\n${JSON.stringify(result[0])}`);
+                pushMessage('title', 'body', 'topic');
             }).catch((e) => {
                 console.log(e)
             }).finally(() => {
@@ -38,4 +42,29 @@ export default async function createPushNotification(promisePool, jPushItem) {
             // Insert push data: toUserId, pushType, pushTime, fromSource, content
         })
     }
+}
+
+const pushMessage = (title, body, topic) => {
+    if (initialized == false) {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+        });
+        initialized = true;
+    }
+
+    const messaging = admin.messaging()
+    var payload = {
+        notification: {
+            title: `${title}`,
+            body: `${body}`
+        },
+        // topic: `/topics/all`,
+        token: 'cBc9Y9R-RvNjhjiOVQTgTs:APA91bEsD4z0Gpkre5t0B4mtgRH56kKYWg0Go5VcS3nTJrCLihA0EoQqjYu5WN1gl9DPczexm2x92q0yXq17XYX9j6GCLFvbrXe66wVXKdXbm9NxVZ5CA3_2xvo_jLoE-TwQ-tPYqtws',
+        // condition: ''
+    };
+
+    messaging.send(payload)
+        .then((result) => {
+            console.log(result)
+        })
 }
