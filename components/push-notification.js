7b819e2aeb3f48fb239b25157cb24ee8ccedbf16
Hyunbo Sim <hbsim0605@gmail.com>
2023-07-29 10:59:11 +0900

    refactoring

--- components/push-notification.js
+++ components/push-notification.js
@@ -7,10 +7,9 @@ import serviceAccount from "./notification/brunner-push-7e0ef-firebase-adminsdk-
 import * as TB_COR_NOTIFICATION_ITEM from './database/sqls/TB_COR_NOTIFICATION_ITEM'
 import * as TB_COR_USER_MST from './database/sqls/TB_COR_USER_MST'
 
-/* 사용자에게 푸시 알람을 생성함
-1. 신규 대화항목를 만들었거나 기존 대화항목을 편집했을때 
- - 작성자 본인을 제외하고 해당 메인 대화에 댓글을 달았거나 메인 대화를 작성한 사람에게 (toUserIds)
-2.특정 사용자에게 메시지를 직접 보내고자 할 경우
+/* 사용자에게 푸시 알람을 보냄.
+1. 신규 대화항목를 만들었거나 기존 대화항목을 편집했을때 작성자 본인을 제외하고 해당 메인 대화에 댓글을 달았거나 메인 대화를 작성한 사람(toUserIds)에게 보냄.
+2.특정 사용자에게 메시지를 직접 보냄.
 */
 let initialized = false;
 
